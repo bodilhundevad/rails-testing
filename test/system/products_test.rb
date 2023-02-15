e505ecab444c0b8097a4929ffb528447619aa6ba
Bodil Hundevad <bodil@hundevad.de>
2023-02-16 00:05:20 +0100

    Add save_and_open_screenshot during product creation for debugging

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -14,12 +14,15 @@ class ProductsTest < ApplicationSystemTestCase
   test "lets signed in user create a new product" do
     login_as users(:george) # Warden::TestHelpers from test_helper.rb
     visit "products/new"
+    # save_and_open_screenshot
 
     fill_in "product_name", with: "My new product"
     fill_in "product_tagline", with: "My new tagline"
+    # save_and_open_screenshot
 
     click_on "Create Product"
+    # save_and_open_screenshot
 
-    assert_text "Change your Life: Learn to code"
+    assert_text "My new product"
   end
 end
