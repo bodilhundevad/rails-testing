1672ec9ad477539dfbce491f288eab53f0b38065
Bodil Hundevad <bodil@hundevad.de>
2023-02-15 23:33:09 +0100

    Improve product test - Add new test to display as many cards as products

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -3,7 +3,11 @@ require "application_system_test_case"
 class ProductsTest < ApplicationSystemTestCase
   test "visiting the index" do # this is called a makro it's the same as def test_visiting_the_index
     visit root_url # visit is a method from Capybara
-    save_and_open_screenshot # only works with "launchy" gem installed
     assert_selector "h1", text: "Product" # assert_selector is a method from Capybara, if true then green
+  end
+
+  test "display as many cards as there are products" do
+    visit root_url
+    assert_selector ".card-product", count: Product.count
   end
 end
